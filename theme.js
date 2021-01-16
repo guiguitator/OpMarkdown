3a5b3909cb9e1a09948eafba3d0503c4111a33e7
guiguitator <53449177+guiguitator@users.noreply.github.com>
2021-01-16 11:26:49 +0100

    🎉 Début du projet

---
+++ theme.js
@@ -0,0 +1,17 @@
+var link = document.getElementById("theme");
+
+function LightTheme() {
+	link.setAttribute('href', 'themes/light_theme.css');
+}
+
+function DarkTheme() {
+	link.setAttribute('href', 'themes/dark_theme.css');
+}
+
+function HackingConsole() {
+	link.setAttribute('href', 'themes/hacking_console.css');
+}
+
+function CherryBlossom() {
+	link.setAttribute('href', 'themes/cherry_blossom.css');
+}
